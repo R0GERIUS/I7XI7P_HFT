3d701ddf6cffe50ab3ed7844f08bde957831a4f8
Ádám Keserű <keseru.adam@gmail.com>
2023-05-28 16:19:24 +0200

    Non CRUD implemented for JS Client

--- I7XI7P_SZTGUI_2022232.JSClient/wwwroot/script.js
+++ I7XI7P_SZTGUI_2022232.JSClient/wwwroot/script.js
@@ -8,6 +8,16 @@ let armorIdToUpdate = -1;
 let jobIdToUpdate = -1;
 let weaponIdToUpdate = -1;
 
+let averageDefenceByJob = -1;
+let averageDefence = -1;
+let averageDamageByJob = -1;
+let averageDamage = -1;
+let highestDmgWeaponByGivenRole = "";
+
+let filterJobId = -1;
+let filterRoleId = -1;
+let filterMinDmg = -1;
+
 let mode = "armor";
 
 switch (mode)
@@ -93,6 +103,7 @@ async function start() {
 function switchToArmor() {
     mode = "armor";
     document.getElementById('updateformdiv').style.display = "none";
+    document.getElementById("updatejobiddiv").style.display = "inherit";
     document.getElementById("headerlabel").innerHTML = "Armor Catalog";
     document.getElementById("namelabel").innerHTML = "Armor Name";
     document.getElementById("valuelabel").innerHTML = "Armor Defense";
@@ -103,12 +114,21 @@ function switchToArmor() {
     document.getElementById("updatebutton").textContent = "Update Armor";
     document.getElementById("updatebutton").setAttribute("onclick", "armorUpdate(armorIdToUpdate)");
     document.getElementById("valueth").innerHTML = "Defense";
+    document.getElementById("filterjobdiv").style.display = "flex";
+    document.getElementById("filterrolediv").style.display = "none";
+    document.getElementById("filtermindmgdiv").style.display = "none";
+    document.getElementById("filterjobbutton").style.display = "none";
+    document.getElementById("filterbutton").textContent = "Filter";
+    document.getElementById("filterbutton").setAttribute("onclick", "armorShowAllJobArmors()");
+    document.getElementById("formjobiddiv").style.display = "flex";
+    armorShowAverageDefence();
     armorGetData();
 }
 
 function switchToJob() {
     mode = "job";
     document.getElementById('updateformdiv').style.display = "none";
+    document.getElementById("updatejobiddiv").style.display = "none";
     document.getElementById("headerlabel").innerHTML = "Job Catalog";
     document.getElementById("namelabel").innerHTML = "Job Name";
     document.getElementById("valuelabel").innerHTML = "Job Role";
@@ -119,12 +139,20 @@ function switchToJob() {
     document.getElementById("updatebutton").textContent = "Update Job";
     document.getElementById("updatebutton").setAttribute("onclick", "jobUpdate(jobIdToUpdate)");
     document.getElementById("valueth").innerHTML = "Role";
+    document.getElementById("filterjobdiv").style.display = "none";
+    document.getElementById("filterrolediv").style.display = "flex";
+    document.getElementById("filtermindmgdiv").style.display = "flex";
+    document.getElementById("filterjobbutton").style.display = "flex";
+    document.getElementById("filterbutton").textContent = "Filter Weapons";
+    document.getElementById("filterbutton").setAttribute("onclick", "jobShowAllRoleWeapons()");
+    document.getElementById("formjobiddiv").style.display = "none";
     jobGetData();
 }
 
 function switchToWeapon() {
     mode = "weapon";
     document.getElementById('updateformdiv').style.display = "none";
+    document.getElementById("updatejobiddiv").style.display = "inherit";
     document.getElementById("headerlabel").innerHTML = "Weapon Catalog";
     document.getElementById("namelabel").innerHTML = "Weapon Name";
     document.getElementById("valuelabel").innerHTML = "Weapon Damage";
@@ -135,10 +163,19 @@ function switchToWeapon() {
     document.getElementById("updatebutton").textContent = "Update Weapon";
     document.getElementById("updatebutton").setAttribute("onclick", "weaponUpdate(weaponIdToUpdate)");
     document.getElementById("valueth").innerHTML = "Damage";
+    document.getElementById("filterjobdiv").style.display = "flex";
+    document.getElementById("filterrolediv").style.display = "none";
+    document.getElementById("filtermindmgdiv").style.display = "none";
+    document.getElementById("filterjobbutton").style.display = "none";
+    document.getElementById("filterbutton").textContent = "Filter";
+    document.getElementById("filterbutton").setAttribute("onclick", "weaponShowAllJobWeapons()");
+    document.getElementById("formjobiddiv").style.display = "flex";
+    weaponShowAverageDamage();
     weaponGetData();
 }
 
 // Armor
+// CRUD
 
 async function armorGetData() {
     await fetch('http://localhost:30703/armor')
@@ -150,6 +187,7 @@ async function armorGetData() {
 }
 
 function armorDisplay() {
+    document.getElementById("valueth").innerHTML = "Defense";
     document.getElementById('resultarea').innerHTML = "";
     armors.forEach(t => {
         document.getElementById('resultarea').innerHTML +=
@@ -171,6 +209,7 @@ function armorRemove(id) {
         .then(response => response)
         .then(data => {
             console.log('Success:', data);
+            armorShowAverageDefence();
             armorGetData();
         })
         .catch((error) => { console.error('Error:', error); });
@@ -180,6 +219,7 @@ function armorRemove(id) {
 function armorShowUpdate(id) {
     document.getElementById("updatename").value = armors.find(t => t['id'] == id)['name'];
     document.getElementById("updatevalue").value = armors.find(t => t['id'] == id)['baseDefense'];
+    document.getElementById("updatejobid").value = armors.find(t => t['id'] == id)['jobId'];
     document.getElementById('updateformdiv').style.display = "flex";
     armorIdToUpdate = id;
 }
@@ -191,11 +231,12 @@ function armorCreate() {
         method: 'POST',
         headers: { 'Content-Type': 'application/json', },
         body: JSON.stringify(
-            { name: name, baseDefense: value })
+            { name: name, baseDefense: value, jobId: 2 })
     })
         .then(response => response)
         .then(data => {
             console.log('Success:', data);
+            armorShowAverageDefence();
             armorGetData();
         })
         .catch((error) => { console.error('Error:', error); });
@@ -205,22 +246,64 @@ function armorCreate() {
 function armorUpdate() {
     let name = document.getElementById('updatename').value;
     let value = document.getElementById('updatevalue').value;
+    let jobId = document.getElementById('updatejobid').value;
     fetch('http://localhost:30703/armor', {
         method: 'PUT',
         headers: { 'Content-Type': 'application/json', },
         body: JSON.stringify(
-            { id: armorIdToUpdate, name: name, baseDefense: value})
+            { id: armorIdToUpdate, name: name, baseDefense: value, jobId: jobId })
     })
         .then(response => response)
         .then(data => {
             console.log('Success:', data);
+            armorShowAverageDefence();
             armorGetData();
         })
         .catch((error) => { console.error('Error:', error); });
 
 }
 
+// Non CRUD
+
+async function armorGetAllJobArmors(jobId) {
+    await fetch('http://localhost:30703/armor/getalljobarmors/' + jobId)
+        .then(x => x.json())
+        .then(y => {
+            armors = y;
+            armorDisplay();
+        });
+}
+
+async function armorGetAverageDefenceByClass(jobId) {
+    await fetch('http://localhost:30703/armor/getaveragedefencebyclass/' + jobId)
+        .then(x => x.json())
+        .then(y => {
+            averageDefenceByJob = y;
+        });
+}
+
+async function armorGetAverageDefence() {
+    await fetch('http://localhost:30703/armor/getaveragedefence')
+        .then(x => x.json())
+        .then(y => {
+            averageDefence = y[0].value;
+        });
+}
+
+async function armorShowAverageDefence() {
+    await armorGetAverageDefence();
+    document.getElementById("headerlabel").innerHTML = "Armor Catalog | Average Defence: " + averageDefence;
+}
+
+async function armorShowAllJobArmors() {
+    let id = document.getElementById("filterjobid").value;
+    armorGetAllJobArmors(id);
+    await armorGetAverageDefenceByClass(id);
+    document.getElementById("headerlabel").innerHTML = "[" + id +"] Armor Catalog | Average Defence: " + averageDefenceByJob;
+}
+
 // Job
+// CRUD
 
 async function jobGetData() {
     await fetch('http://localhost:30703/job')
@@ -232,6 +315,7 @@ async function jobGetData() {
 }
 
 function jobDisplay() {
+    document.getElementById("valueth").innerHTML = "Role";
     document.getElementById('resultarea').innerHTML = "";
     jobs.forEach(t => {
         document.getElementById('resultarea').innerHTML +=
@@ -302,7 +386,65 @@ function jobUpdate() {
 
 }
 
+// Non CRUD
+
+async function jobGetAllJobsByRole(roleId) {
+    await fetch('http://localhost:30703/job/getalljobsbyrole/' + roleId)
+        .then(x => x.json())
+        .then(y => {
+            jobs = y;
+            jobDisplay();
+        });
+}
+
+async function jobGetAllWeaponByRole(roleId) {
+    await fetch('http://localhost:30703/job/getallweaponbyrole/' + roleId)
+        .then(x => x.json())
+        .then(y => {
+            weapons = y;
+            weaponDisplay();
+        });
+}
+
+async function jobGetAllWeaponByRoleMinimumDmg(roleId, minDmg) {
+    await fetch('http://localhost:30703/job/getallweaponbyroleminimumdmg/' + roleId + '/' + minDmg)
+        .then(x => x.json())
+        .then(y => {
+            weapons = y;
+            weaponDisplay();
+        });
+}
+
+async function jobGetHighestDMGWeaponGivenRole(roleId) {
+    await fetch('http://localhost:30703/job/gethighestdmgweapongivenrole/' + roleId)
+        .then(x => x.json())
+        .then(y => {
+            highestDmgWeaponByGivenRole = y[0];
+        });
+}
+
+async function jobShowAllRoleWeapons() {
+    let roleId = document.getElementById("filterroleid").value;
+    let minDmg = document.getElementById("filtermindmg").value;
+    if (minDmg > 0) {
+        jobGetAllWeaponByRoleMinimumDmg(roleId, minDmg);
+    }
+    else
+    {
+        jobGetAllWeaponByRole(roleId);
+    }
+    await jobGetHighestDMGWeaponGivenRole(roleId);
+    document.getElementById("headerlabel").innerHTML = "[" + roleId + "] Job Weapons | Best Weapon: " + highestDmgWeaponByGivenRole.name + " (" + highestDmgWeaponByGivenRole.baseDamage + ")";
+}
+
+async function jobShowAllJobsByRole() {
+    let roleId = document.getElementById("filterroleid").value;
+    jobGetAllJobsByRole(roleId);
+    document.getElementById("headerlabel").innerHTML = "[" + roleId + "] Job Catalog";
+}
+
 // Weapon
+// CRUD
 
 async function weaponGetData() {
     await fetch('http://localhost:30703/weapon')
@@ -314,6 +456,7 @@ async function weaponGetData() {
 }
 
 function weaponDisplay() {
+    document.getElementById("valueth").innerHTML = "Damage";
     document.getElementById('resultarea').innerHTML = "";
     weapons.forEach(t => {
         document.getElementById('resultarea').innerHTML +=
@@ -335,6 +478,7 @@ function weaponRemove(id) {
         .then(response => response)
         .then(data => {
             console.log('Success:', data);
+            weaponShowAverageDamage();
             weaponGetData();
         })
         .catch((error) => { console.error('Error:', error); });
@@ -342,24 +486,27 @@ function weaponRemove(id) {
 }
 
 function weaponShowUpdate(id) {
-    weaponToUpdate = weapons.find(t => t['id'] == id);
-    document.getElementById("updatename").value = weaponToUpdate['name'];
-    document.getElementById("updatevalue").value = weaponToUpdate['baseDamage'];
+    document.getElementById("updatename").value = weapons.find(t => t['id'] == id)['name'];
+    document.getElementById("updatevalue").value = weapons.find(t => t['id'] == id)['baseDamage'];
+    document.getElementById("updatejobid").value = weapons.find(t => t['id'] == id)['jobId'];
     document.getElementById('updateformdiv').style.display = "flex";
+    weaponIdToUpdate = id;
 }
 
 function weaponCreate() {
     let name = document.getElementById('name').value;
     let value = document.getElementById('value').value;
+    let jobId = document.getElementById('jobId').value;
     fetch('http://localhost:30703/weapon', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json', },
         body: JSON.stringify(
-            { name: name, baseDamage: value })
+            { name: name, baseDamage: value, jobId: jobId })
     })
         .then(response => response)
         .then(data => {
             console.log('Success:', data);
+            weaponShowAverageDamage();
             weaponGetData();
         })
         .catch((error) => { console.error('Error:', error); });
@@ -369,17 +516,58 @@ function weaponCreate() {
 function weaponUpdate() {
     let name = document.getElementById('updatename').value;
     let value = document.getElementById('updatevalue').value;
+    let jobId = document.getElementById('updatejobId').value;
     fetch('http://localhost:30703/weapon', {
         method: 'PUT',
         headers: { 'Content-Type': 'application/json', },
         body: JSON.stringify(
-            { id: armorIdToUpdate, name: name, baseDamage: value })
+            { id: weaponIdToUpdate, name: name, baseDamage: value, jobId: jobId })
     })
         .then(response => response)
         .then(data => {
             console.log('Success:', data);
+            weaponShowAverageDamage();
             weaponGetData();
         })
         .catch((error) => { console.error('Error:', error); });
 
+}
+
+// Non CRUD
+
+async function weaponGetAllJobWeapons(jobId) {
+    await fetch('http://localhost:30703/weapon/getalljobweapons/' + jobId)
+        .then(x => x.json())
+        .then(y => {
+            weapons = y;
+            weaponDisplay();
+        });
+}
+
+async function weaponGetAverageDamageByJob(jobId) {
+    await fetch('http://localhost:30703/weapon/getaveragedamagebyjob/' + jobId)
+        .then(x => x.json())
+        .then(y => {
+            averageDamageByJob = y;
+        });
+}
+
+async function weaponGetAverageDamage() {
+    await fetch('http://localhost:30703/weapon/getaveragedamage')
+        .then(x => x.json())
+        .then(y => {
+            averageDamage = y[0].value;
+        });
+}
+
+async function weaponShowAverageDamage() {
+    await weaponGetAverageDamage();
+    document.getElementById("headerlabel").innerHTML = "Weapon Catalog | Average Damage: " + averageDamage;
+}
+
+async function weaponShowAllJobWeapons() {
+    let id = document.getElementById("filterjobid").value;
+    weaponGetAllJobWeapons(id);
+    await weaponGetAverageDefenceByClass(id);
+    document.getElementById("headerlabel").innerHTML = "[" + id + "] Weapon Catalog | Average Damage: " + averageDamageByJob;
 }
