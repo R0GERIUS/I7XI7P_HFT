ba6592405f39c758dfa4d671edd745952def2b5a
Ádám Keserű <keseru.adam@gmail.com>
2023-05-28 10:31:18 +0200

    Basic JS client implementation

--- I7XI7P_SZTGUI_2022232.JSClient/Program.cs
+++ I7XI7P_SZTGUI_2022232.JSClient/Program.cs
@@ -1,5 +1,19 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.WithOrigins("http://localhost:5259")
+                .AllowAnyHeader()
+                .AllowAnyMethod()
+                .AllowCredentials();
+    });
+});
+
 var app = builder.Build();
+
+app.UseCors();
 
 app.UseStaticFiles();
 
---
+++ I7XI7P_SZTGUI_2022232.JSClient/wwwroot/script.js
@@ -0,0 +1,91 @@
+﻿let armors = [];
+let jobs = [];
+let weapons = [];
+let connection = null;
+
+getdata();
+setupSignalR();
+
+function setupSignalR() {
+    connection = new signalR.HubConnectionBuilder()
+        .withUrl("http://localhost:30703/hub")
+        .configureLogging(signalR.LogLevel.Information)
+        .build();
+
+    connection.on("ArmorCreated", (user, message) => {
+        getdata();
+    });
+
+    connection.on("ArmorDeleted", (user, message) => {
+        getdata();
+    });
+
+    connection.onclose(async () => {
+        await start();
+    });
+    start();
+
+
+}
+
+async function start() {
+    try {
+        await connection.start();
+        console.log("SignalR Connected.");
+    } catch (err) {
+        console.log(err);
+        setTimeout(start, 5000);
+    }
+};
+
+async function getdata() {
+    await fetch('http://localhost:30703/armor')
+        .then(x => x.json())
+        .then(y => {
+            armors = y;
+            display();
+        });
+}
+
+function display() {
+    document.getElementById('resultarea').innerHTML = "";
+    armors.forEach(t => {
+        document.getElementById('resultarea').innerHTML +=
+            "<tr><td>" + t.armorName + "</td><td>"
+            + t.armorDefense + "</td><td>" +
+            `<button type="button" onclick="remove(${t.armorId})">Delete</button>`
+            + "</td></tr>";
+    });
+}
+
+function remove(id) {
+    fetch('http://localhost:30703/armor/' + id, {
+        method: 'DELETE',
+        headers: { 'Content-Type': 'application/json', },
+        body: null
+    })
+        .then(response => response)
+        .then(data => {
+            console.log('Success:', data);
+            getdata();
+        })
+        .catch((error) => { console.error('Error:', error); });
+
+}
+
+function create() {
+    let name = document.getElementById('actorname').value;
+    fetch('http://localhost:30703/armor', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json', },
+        body: JSON.stringify(
+            { actorName: name })
+    })
+        .then(response => response)
+        .then(data => {
+            console.log('Success:', data);
+            getdata();
+        })
+        .catch((error) => { console.error('Error:', error); });
+
+}
